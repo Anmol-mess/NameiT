59d9a2f98c5b0861aa34887ddda835a09c530093
Anmol-mess <61362334+Anmol-mess@users.noreply.github.com>
2021-06-20 16:25:01 +0530

    aDD

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
+import {Browser as Router} from 'react-router-dom';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById('root')
 );
 
